fde13df91a4e52558091c0a8c81e084b513f8a35
narayank97 <narayankarun@gmail.com>
2018-12-25 20:13:14 -0800

    added permutation checker

---
+++ permCheck.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string.h>
+#include <stdbool.h>
+
+using namespace std;
+
+int indexGen(char letter)
+{
+    int index;
+    index = 0; // THIS CHUNK USED TO make cpaital letters lowercase
+    int myLetter = (int) letter;
+    if(myLetter < 97)
+    {
+        myLetter += 32;
+    }
+    return index = myLetter - 97;
+
+}
+
+bool permCheck(string word1, string word2) 
+{
+    int* myWord1 = new int[26];
+    int* myWord2 = new int[26];
+
+    memset(myWord1,0,26);
+    memset(myWord2,0,26);
+
+    int indexWord1 = 0;
+    int indexWord2 = 0;
+
+
+    if(word1.length() != word2.length())
+    {
+        return false;
+    }
+    
+    for(int i = 0; i < word1.length(); i++) // initialize each element to 0
+    {
+        indexWord1 = 0;
+        indexWord2 = 0;
+        indexWord1 = indexGen(word1[i]);
+        indexWord2 = indexGen(word2[i]);
+        myWord1[indexWord1]+=1;
+        myWord2[indexWord2]+=1;
+    }
+    for(int j = 0; j < 26; j++)
+    {
+        if(myWord1[j] != myWord2[j])
+        {
+            return false;
+        }
+        continue;
+    }
+    return true;
+   
+}
+
+
+int main()
+{
+    cout << permCheck("karun","naruk") << endl;
+    cout << permCheck("abc","deg") << endl;
+    return 0;
+}
